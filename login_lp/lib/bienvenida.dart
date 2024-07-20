f18533b76f022dedbbb502dae4390a27de8b759a
FjC-14 <fcastrol@unah.hn>
2024-07-19 18:22:57 -0600

    Rework home

--- login_lp/lib/bienvenida.dart
+++ login_lp/lib/bienvenida.dart
@@ -7,51 +7,72 @@ class Bienvenida extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey[50], 
       body: Center(
-        child: Card.outlined(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Bienvenido',
-                style: TextStyle(fontSize: 50),
-              ),
-              const SizedBox(height: 20),
-              const Icon(
-                Icons.account_circle,
-                size: 150,
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                     ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, MyRoutes.inicio.name);
-                },
-                icon: const Icon(Icons.person_2), 
-                label: const Text('Iniciar sesión'),
-                style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(175, 50), 
-                  ), 
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          elevation: 8, 
+          margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Bienvenido',
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueGrey,
                   ),
-                ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, MyRoutes.registro.name);
-                },
-                icon: const Icon(Icons.login), 
-                label: const Text('Registrarse'),
-                style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(175, 50), 
-                  ), 
-                  ),
-                ],
-              )
-            ],
+                ),
+                const SizedBox(height: 20),
+                const Icon(
+                  Icons.account_circle,
+                  size: 150,
+                  color: Colors.blueGrey,
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, MyRoutes.inicio.name);
+                      },
+                      icon: const Icon(Icons.person_2), 
+                      label: const Text('Iniciar sesión'),
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white, backgroundColor: Colors.blueGrey, minimumSize: const Size(160, 50), 
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, MyRoutes.registro.name);
+                      },
+                      icon: const Icon(Icons.login), 
+                      label: const Text('Registrarse'),
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white, minimumSize: const Size(160, 50), backgroundColor: Colors.blueGrey, 
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12), 
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
-
     );
   }
 }
